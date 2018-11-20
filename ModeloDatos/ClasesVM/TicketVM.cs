241f62e8f821d801fa7379c2b7fd5d4d6a92e316
Julian Dolz Moncho <juliandm69@gmail.com>
2018-11-21 00:40:50 +0100

    Creados modelo y codigo de ventana de la pantalla cobrar (DataContext)

---
+++ ModeloDatos/ClasesVM/TicketVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatos.ClasesVM
+{
+    public class TicketVM : VMBase 
+    {
+        public TBLTICKETS ElTicket { get; set; }
+        public TicketVM()
+        {
+            ElTicket = new TBLTICKETS();
+        }
+    }
+}
---
+++ Veterinaria/Vistas/Cobrar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Veterinaria.CodigoDelegado;
+
+namespace Veterinaria.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Cobrar.xaml
+    /// </summary>
+    public partial class Cobrar : UserControl
+    {
+        CobrarViewModel viewModel;
+        public Cobrar()
+        {
+            InitializeComponent();
+            viewModel = new CobrarViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
